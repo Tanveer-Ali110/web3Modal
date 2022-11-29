63e944ccd53a873bc134a5f2f7ed8a20d013e2d8
Tanveer Ali <ali.tanveer1012@gmail.com>
2022-11-29 14:17:22 +0500

    update

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -5,6 +5,7 @@ import { useWeb3ModalProvider } from "./useWeb3Modal";
 
 export const useSigner = () => {
   const { library, account, web3NoAccount, chainId } = useWeb3ModalProvider();
+  
   const signer = useMemo(
     () => (chainId ? library?.getSigner(account) : web3NoAccount),
     [chainId, library, account, web3NoAccount]
