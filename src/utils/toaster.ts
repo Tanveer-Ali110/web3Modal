f1f6b21a6559d508cd8b18b23d7bf068446b8115
Tanveer Ali <ali.tanveer1012@gmail.com>
2022-12-28 18:53:00 +0500

    change

--- src/utils/toaster.ts
+++ src/utils/toaster.ts
@@ -1,19 +1,15 @@
 import Toast, { toastTypes } from "component/Toast"
-import Example from "component/Toast1"
-
+import Toast1 from "component/Toast1"
 
 export const toastSuccess = (title?: string, description?: string) => {
-    return Example({ type: toastTypes.success, title, description })
+    return Toast1({ type: toastTypes.success, title, description })
 }
-// export const toastSuccess = (title: string, description?: string) => {
-//     return Toast({ type: toastTypes.success, title, description })
-// }
 export const toastError = (title: string, description?: string) => {
-    return Example({ type: toastTypes.error, title, description })
+    return Toast1({ type: toastTypes.error, title, description })
 }
 export const toastInfo = (title: string, description?: string) => {
-    return Example({ type: toastTypes.info, title, description })
+    return Toast1({ type: toastTypes.info, title, description })
 }
 export const toastWarnig = (title: string, description?: string) => {
-    return Example({ type: toastTypes.warning, title, description })
+    return Toast1({ type: toastTypes.warning, title, description })
 }
